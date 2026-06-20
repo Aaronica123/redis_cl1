148f5d27376f566d8f7352f8aae7f3a96ced3d85
Aaronica123 <k.aaronmutua@gmail.com>
2026-06-20 20:06:19 +0300

    redis client

--- prj/files/redis_files/redis_pool.js
+++ prj/files/redis_files/redis_pool.js
@@ -1,9 +1,9 @@
 import redis from 'redis';
 const pl=redis.createClientPool({
-    host:"redis-server",
-    port:6379,
-    maxclients:2,
+     url: 'redis://localhost:6379',
+    maxClients:5,
 })
+
 const in1=redis.createClient({
     host:"redis-server",
     port:6379
@@ -11,18 +11,22 @@ const in1=redis.createClient({
 const cl_in=async()=>{
     const cn=await in1.connect();
     if(cn){
-        console.log("independent server");
+        console.log("independent server" + in1);
     }
     else{
         console.log("server not connected");
     }
     
 }
+var c=1;
 
 const y=async()=>{
-    const conn=await pl.connect();
+    const conn = pl;
+    
      if(conn){
-        console.log("connection created 1")
+        console.log("connection created 1");
+         console.log(conn);
+         
     }
     else{
         console.log("denied created")
@@ -30,40 +34,48 @@ const y=async()=>{
 }
 
 const x=async()=>{
+   
     const conn=await pl.connect();
     if(conn){
-        console.log("connection created 2")
+        console.log("connection created 2");
+         console.log(c+1);
     }
     else{
-        console.log("denied created")
+        console.log("denied created");
+        console.log(conn);
     }
 }
-const z=async()=>{
-    const conn=await pl.connect();
-    if(conn){
-        console.log("connection created 3")
-    }
-    else{
-        console.log("denied created")
-    }
+// const z=async()=>{
+//     const conn=await pl.connect();
+//     if(conn){
+//         console.log("connection created 3")
+//         console.log(c++);
+//     }
+//     else{
+//         console.log("denied created")
+//     }
     
-}
-const q=async()=>{
-    const conn=await pl.connect();
-    if(conn){
-        console.log("connection created 4")
-    }
-    else{
-        console.log("denied request")
-    }
+// }
+// const q=async()=>{
+//     const conn=await pl.connect();
+//     if(conn){
+//         console.log("connection created 4")
+//          console.log(c++);
+//     }
+//     else{
+//         console.log("denied request")
+//     }
     
-}
-function call(){
-    y();
-    x();
-    cl_in(); 
+// }
+async function call(){
+    await Promise.all([
+    y(),
+    x(),
+    cl_in(), 
     // cl_in1();
-    z();
-    q();
+    // z(),
+    // q()
+    ]);
+    
 }
 export default call;
